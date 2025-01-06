7ec3eb6139515e7b04f7f76537edfd90875a192c
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-01-06 09:41:03 +0530

    Update DisjointSetUnion.cpp

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -1,55 +1,60 @@
-class DisjointSet {
-    vector<int>rank,size,prnt;
+#include<bits/stdc++.h>
+using namespace std;
+class DisjointSetUnion
+{
 public:
-    DisjointSet(int n) {
-        rank.resize(n+1,0);
+    vector<int>prnt,size;
+    DisjointSetUnion(int n)
+    {
+        prnt.resize(n+1,0);
         size.resize(n+1,1);
-        prnt.resize(n+1);
         for(int i=0;i<=n;++i)
             prnt[i]=i;
     }
-
     int findUPar(int u)
     {
-        if(u==prnt[u])
+        if(prnt[u]==u)
             return u;
-        return prnt[u]=findUPar(prnt[u]);
+        else
+            return prnt[u]=findUPar(prnt[u]);
     }
-
-    bool find(int u, int v) {
-        return findUPar(u)==findUPar(v);
-    }
-
-    void unionByRank(int u, int v) {
+    void UnionBySize(int u,int v)
+    {
         int ulp_u=findUPar(u);
         int ulp_v=findUPar(v);
         if(ulp_u==ulp_v)
             return ;
-        if(rank[ulp_u]<rank[ulp_v])
-            prnt[ulp_u]=ulp_v;
-        else if(rank[ulp_u]>rank[ulp_v])
-            prnt[ulp_v]=ulp_u;
-        else
+        if(size[ulp_u]>size[ulp_v])
         {
             prnt[ulp_v]=ulp_u;
-            ++rank[ulp_u];
+            size[ulp_u]+=size[ulp_v];
+        }
+        else
+        {
+            prnt[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
         }
     }
-
-    void unionBySize(int u, int v) {
-       int ulp_u=findUPar(u);
-            int ulp_v=findUPar(v);
-            if(ulp_u==ulp_v)    
-                return ;
-            if(size[ulp_u]<size[ulp_v])
-            {
-                prnt[ulp_u]=ulp_v;
-                size[ulp_v]+=size[ulp_u];
-            }
-            else
-            {
-                prnt[ulp_v]=ulp_u;
-                size[ulp_u]+=size[ulp_v];
-            }
-    }
 };
+
+int main()
+{
+    DisjointSetUnion dsu(7);
+    dsu.UnionBySize(0,1);
+    dsu.UnionBySize(2,1);
+    dsu.UnionBySize(2,5);
+    dsu.UnionBySize(2,3);
+    dsu.UnionBySize(1,3);
+    dsu.UnionBySize(3,5);
+    dsu.UnionBySize(3,7);
+    dsu.UnionBySize(4,6);
+    if(dsu.findUPar(2)==dsu.findUPar(6))
+        cout<<"Same component.\n";
+    else    
+        cout<<"Not Same.\n";
+    dsu.UnionBySize(2,4);
+    if(dsu.findUPar(2)==dsu.findUPar(6))
+        cout<<"Same component.\n";
+    else    
+        cout<<"Not Same.\n";
+}
